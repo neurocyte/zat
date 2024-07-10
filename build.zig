2e948281339eb1627b84a45c3acf336d1c98a307
CJ van den Berg <cj@vdbonline.com>
2024-07-10 22:10:59 +0200

    feat: update to zig-0.13 and update flow file types and themes

--- build.zig
+++ build.zig
@@ -12,7 +12,7 @@ pub fn build(b: *std.Build) void {
 
     const exe = b.addExecutable(.{
         .name = "zat",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
